d293d159dbd808a71b50f9b4f91bc4119a00ea15
G Deepak <54737151+gdeepakdeepuml@users.noreply.github.com>
2021-02-13 13:26:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,70 @@ import streamlit.components.v1 as components
 import time
 from PIL import Image
 import joblib
+import torch 
+import random 
+import json
+import torch 
+
+from model import NeuralNet
+from nltk_util import tokenization,stemm,bag_of_word
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+with open('intentscovid.json','r') as f:
+    intents = json.load(f)
+
+File = 'datacovid.pth'
+data = torch.load(File)
+
+input_size = data['input_size']
+output_size = data['output_size']
+hidden_size = data['hidden_size']
+model_state = data['model_state']
+all_words = data['all_words']
+tags = data ['tags']
+
+model = NeuralNet(input_size,hidden_size,output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+
+bot_name = 'covidbot'
+#print("do you help to protect from covid!! let's chat! type 'quit' to get out !! ")
+
+def chatpost(msg):
+#while True:
+    #sentence = input("You: ")
+    print(msg)
+    sentence = msg
+    if sentence.lower() == "quit":
+        print("its work's") 
+
+    sentence = tokenization(sentence)
+    x = bag_of_word(sentence,all_words)
+    x = x.reshape(1, x.shape[0])
+    x = torch.from_numpy(x)
+
+    output = model(x)
+    _, predicted = torch.max(output,dim=1)
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+
+    if prob.item() > 0.65:
+        for intent in intents['intents']:
+            if tag == intent['tag']:
+                responses = intent['responses']
+                
+        return (random.choice(responses))
+    else:
+        answer="I don't understand can you be more clear please!"
+        return (answer)
+
+
+
 st.set_page_config(page_title="ML-Doctor",page_icon='⚕️',layout="centered",initial_sidebar_state="collapsed",)
 
 def predict_function(to_predict_list, size):
@@ -58,8 +122,11 @@ def predict_function(to_predict_list, size):
         else:
             return "stay Happy !you have no Kidney Disease"
         
-            
-
+# here coms the maln code of API  
+## this for Covid-19 Bot            
+def get_text():
+    input_text = st.text_input("You Question :")
+    return input_text 
 
 def main():
     heading = """
@@ -469,9 +536,21 @@ def main():
             
         st.subheader(result)
         st.markdown(html_temp_liver,unsafe_allow_html=True)
-        
-        
+    
+    st.subheader("The Chat-Bot for COVID-19")
+    st.write("COVID-19 affects different people in different ways. Most infected people will develop mild to moderate illness and recover without hospitalization. here the CovidBot which can help to answer any question about COVID-19")
+    
+    user_input = get_text()
+    result = chatpost(user_input)
 
+    #user_input_list.append(user_input)
+    #bible_ans.append(Initialize botchatpost(user_input))
+    if st.button("send"):
+        st.text_area("Covid-19 Bot Say's :",value=result,
+                    height=40,max_chars=None,key=None)
+    else:
+        st.text_area("Covid-19 Bot Say's :",value="Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus. i can help You to be safe and answer your question",
+                    height=30,max_chars=None,key=None)
     
     #####################
     st.subheader("Creator:")
@@ -481,5 +560,3 @@ def main():
         
 if __name__=='__main__':
     main()
-    
-    
